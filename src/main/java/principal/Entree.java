896f2113493ccf518834bb1de84252279285e342
crespo <matthiascrespo9@gmail.com>
2021-03-15 17:58:42 +0100

    j'ai ajouter le if et des int

---
+++ src/main/java/principal/Entree.java
@@ -0,0 +1,41 @@
+package principal;
+import java.util.Scanner;
+
+public class Entree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nombre mystère ");
+		System.out.println("1. Deviner");
+		System.out.println("2. Faire deviner l'ordinateur");
+		Scanner scanner = new Scanner(System.in);
+		int choix = scanner.nextInt();
+		if (choix == 1) {
+			
+			int nbMys;
+			int nbChoix;
+			while() {
+				
+			}
+			
+			if (proposition > nbMys) {
+				System.out.println("Trop grand +");
+			}
+			else if(proposition < nbMys) {
+				System.out.println("trop petit -");
+			}
+			else {
+				System.out.println("bravo");
+			}
+			
+		}
+		else if (choix == 2)
+		{
+			
+		}
+		else {
+			
+		}
+	}
+
+}
