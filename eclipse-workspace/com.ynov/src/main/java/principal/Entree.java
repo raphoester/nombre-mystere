33708316df17e0c968c61fdf11d12beb4bf139d8
Raphaël Oester <raphael.oester@ynov.com>
2021-03-15 16:17:16 +0100

    menu principal

---
+++ eclipse-workspace/com.ynov/src/main/java/principal/Entree.java
@@ -0,0 +1,25 @@
+package principal;
+import java.util.Scanner;
+
+public class Entree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nombre mystère ");
+		System.out.println("1. Deviner");
+		System.out.println("2. Faire deviner l'ordinateur");
+		Scanner scanner = new Scanner(System.in);
+		int choix = scanner.nextInt();
+		if (choix == 1) {
+			
+		}
+		else if (choix == 2)
+		{
+			
+		}
+		else {
+			
+		}
+	}
+
+}
